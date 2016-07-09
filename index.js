9e449ea5b31dfc6c06ec4962f93a0c0dd7c85c72
SPATIL5 <swapnil_patil@intuit.com>
2016-07-09 15:10:03 -0700

    dd

--- index.js
+++ index.js
@@ -1,67 +1,202 @@
-'use strict'
+'use strict';
 
+const apiai = require('apiai');
 const express = require('express');
 const bodyParser = require('body-parser');
+const uuid = require('node-uuid');
 const request = require('request');
-const app = express();
-const token = "EAAI3np0nrmYBAAPDyQglzsz1bEJTQ4JHZAStdZC3EivStE7IBknZCZChTOwvRBjE9ZAny0iNyrw3DuYCAdmqzsOby7oJVtNjS4w5GsDPLDWCZCIjMBnvuEImgBcTbY6FRZCxaDIPYh5RaLmffrddCG43cbZA8HsWqYnNJSm5mfoiFgZDZD";
-app.set('port', (process.env.PORT || 5000));
+const JSONbig = require('json-bigint');
+const async = require('async');
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
+const REST_PORT = (process.env.PORT || 5000);
+const APIAI_ACCESS_TOKEN = "aae85be98a3747b2ae00b8f9654e3ab4";
+const APIAI_LANG = 'en';
+const FB_VERIFY_TOKEN = "i_need_help_here";
+const FB_PAGE_ACCESS_TOKEN = "EAAZAqQj1VP6wBAOjsswZAQFIBBfCo482jaMG4489l9GsWTMHZBIyKQvXGyhI0vtEj6RsrnpY5ZBHaDAzyyZC5OyYEoyxGTmLmtSZAqm1U73IQ4QqzxnpCHQ95ykB4DlRNEk4Aqof5lWYAMtAcAaH7nmS4R4s4iwglAMNy2oxylkwZDZD";
 
-// Process application/json
-app.use(bodyParser.json());
+const apiAiService = apiai(APIAI_ACCESS_TOKEN, {language: APIAI_LANG, requestSource: "fb"});
+const sessionIds = new Map();
 
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-});
+function processEvent(event) {
+    var sender = event.sender.id.toString();
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'i_need_help_here') {
-        res.send(req.query['hub.challenge']);
+    if (event.message && event.message.text) {
+        var text = event.message.text;
+        // Handle a text message from this sender
+
+        if (!sessionIds.has(sender)) {
+            sessionIds.set(sender, uuid.v1());
+        }
+
+        console.log("Text", text);
+
+        let apiaiRequest = apiAiService.textRequest(text,
+            {
+                sessionId: sessionIds.get(sender)
+            });
+
+        apiaiRequest.on('response', (response) => {
+            if (isDefined(response.result)) {
+                let responseText = response.result.fulfillment.speech;
+                let responseData = response.result.fulfillment.data;
+                let action = response.result.action;
+
+                if (isDefined(responseData) && isDefined(responseData.facebook)) {
+                    try {
+                        console.log('Response as formatted message');
+                        sendFBMessage(sender, responseData.facebook);
+                    } catch (err) {
+                        sendFBMessage(sender, {text: err.message });
+                    }
+                } else if (isDefined(responseText)) {
+                    console.log('Response as text message');
+                    // facebook API limit for text length is 320,
+                    // so we split message if needed
+                    var splittedText = splitResponse(responseText);
+
+                    async.eachSeries(splittedText, (textPart, callback) => {
+                        sendFBMessage(sender, {text: textPart}, callback);
+                    });
+                }
+
+            }
+        });
+
+        apiaiRequest.on('error', (error) => console.error(error));
+        apiaiRequest.end();
     }
-    res.send('Error, wrong token');
+}
+
+function splitResponse(str) {
+    if (str.length <= 320)
+    {
+        return [str];
+    }
+
+    var result = chunkString(str, 300);
+
+    return result;
+
+}
+
+function chunkString(s, len)
+{
+    var curr = len, prev = 0;
+
+    var output = [];
+
+    while(s[curr]) {
+        if(s[curr++] == ' ') {
+            output.push(s.substring(prev,curr));
+            prev = curr;
+            curr += len;
+        }
+        else
+        {
+            var currReverse = curr;
+            do {
+                if(s.substring(currReverse - 1, currReverse) == ' ')
+                {
+                    output.push(s.substring(prev,currReverse));
+                    prev = currReverse;
+                    curr = currReverse + len;
+                    break;
+                }
+                currReverse--;
+            } while(currReverse > prev)
+        }
+    }
+    output.push(s.substr(prev));
+    return output;
+}
+
+function sendFBMessage(sender, messageData, callback) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: FB_PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+
+        if (callback) {
+            callback();
+        }
+    });
+}
+
+function doSubscribeRequest() {
+    request({
+            method: 'POST',
+            uri: "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=" + FB_PAGE_ACCESS_TOKEN
+        },
+        function (error, response, body) {
+            if (error) {
+                console.error('Error while subscription: ', error);
+            } else {
+                console.log('Subscription result: ', response.body);
+            }
+        });
+}
+
+function isDefined(obj) {
+    if (typeof obj == 'undefined') {
+        return false;
+    }
+
+    if (!obj) {
+        return false;
+    }
+
+    return obj != null;
+}
+
+const app = express();
+
+app.use(bodyParser.text({ type: 'application/json' }));
+
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] == FB_VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge']);
+
+        setTimeout(function () {
+            doSubscribeRequest();
+        }, 3000);
+    } else {
+        res.send('Error, wrong validation token');
+    }
 });
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging;
+    try {
+        var data = JSONbig.parse(req.body);
 
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            sendTextMessage(sender, "echo: " + text.substring(0, 200))
+        var messaging_events = data.entry[0].messaging;
+        for (var i = 0; i < messaging_events.length; i++) {
+            var event = data.entry[0].messaging[i];
+            processEvent(event);
         }
+        return res.status(200).json({
+            status: "ok"
+        });
+    } catch (err) {
+        return res.status(400).json({
+            status: "error",
+            error: err
+        });
     }
-    res.sendStatus(200)
+
 });
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
+app.listen(REST_PORT, function () {
+    console.log('Rest service ready on port ' + REST_PORT);
 });
+
+doSubscribeRequest();
