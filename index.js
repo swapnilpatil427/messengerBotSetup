e3daeb7cecc9cbbbdb13ac9c73472c6070d5cb88
SPATIL5 <swapnil_patil@intuit.com>
2016-07-10 08:36:13 -0700

    fg

--- index.js
+++ index.js
@@ -317,18 +317,18 @@ app.get('/refugee/:id', function(req, res) {
 app.post('/refugee', function(req, res){
     //console.log()
     //res.write("fd");
-    res.end();
-    /*var id = req.params.refugeeID;
+    var id = req.params.refugeeID;
     var address = req.params.refugeeAddress;
     var phoneNumber = req.params.refugeePhone;
+    console.log(phoneNumber + address);
     con.query('CALL insert_refugee(' + id +',kartik,'+ address +',95112,' +phoneNumber +',25,male,no,37.383411,121.919662)', function(err, rows) {
         if (err) {
             console.log(err);
         }
         console.log(rows);
-        res.write(JSON.stringify(rows));
+    //    res.write(JSON.stringify(rows));
         res.end();
-    }); */
+    });
 });
 
 app.post('/sms', (req, res) => {
