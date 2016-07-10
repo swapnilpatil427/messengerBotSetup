63192e40d502c699c54b0f9554c7e40c5faddeda
SPATIL5 <swapnil_patil@intuit.com>
2016-07-10 12:07:10 -0700

    dfgfd

--- twiliobot.js
+++ twiliobot.js
@@ -114,7 +114,7 @@ module.exports = class TwilioBot {
                                                     elements = "Name : " + row.name + "Description :" + row.description + "phoneNumber :" + row.phone + "\n";
                                                 });
                                             }
-                                            
+
                                         });
 
                                     });
@@ -122,7 +122,7 @@ module.exports = class TwilioBot {
                             }
                             console.log('Response as text message');
                             res.setHeader("Content-Type", "application/xml");
-                            res.status(200).end("<Response><Message>" + xmlescape(responseText) + "\n" +xmlescape(elements) + "</Message></Response>");
+                            res.status(200).end("<Response><Message>" + xmlescape(responseText)+  "</Message></Response>");
                         } else {
                             console.log('Received empty speech');
                         }
