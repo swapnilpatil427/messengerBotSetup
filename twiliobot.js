bf188269bcacd1336aaecf55e176cf58d289e196
SPATIL5 <swapnil_patil@intuit.com>
2016-07-10 03:24:03 -0700

    fg

--- twiliobot.js
+++ twiliobot.js
@@ -4,6 +4,7 @@ const apiai = require('apiai');
 const uuid = require('node-uuid');
 const request = require('request');
 const xmlescape = require('xml-escape');
+var apiaiRequests = require('./index.js');
 
 module.exports = class TwilioBot {
 
@@ -58,32 +59,12 @@ module.exports = class TwilioBot {
                     this._sessionIds.set(chatId, uuid.v1());
                 }
 
-                let apiaiRequest = this._apiaiService.textRequest(messageText,
-                    {
-                        sessionId: this._sessionIds.get(chatId)
-                    });
-
-                apiaiRequest.on('response', (response) => {
-                    console.log(response);
-                    if (TwilioBot.isDefined(response.result)) {
-                        let responseText = response.result.fulfillment.speech;
-
-                        if (TwilioBot.isDefined(responseText)) {
-                            console.log('Response as text message');
-                            res.setHeader("Content-Type", "application/xml");
-                            res.status(200).end("<Response><Message>" + xmlescape(responseText) + "</Message></Response>");
-                        } else {
-                            console.log('Received empty speech');
-                        }
-                    } else {
-                        console.log('Received empty result')
-                    }
+                let apiaiRequest = this._apiaiService.textRequest(messageText, {
+                    sessionId: this._sessionIds.get(chatId)
                 });
 
-                apiaiRequest.on('error', (error) => console.error(error));
-                apiaiRequest.end();
-            }
-            else {
+                apiaiRequests();
+            } else {
                 console.log('Empty message');
                 return res.status(400).end('Empty message');
             }
