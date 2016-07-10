a6ec3767d44b0ee3258a40d110a5b6fcd56b2f73
SPATIL5 <swapnil_patil@intuit.com>
2016-07-09 21:16:13 -0700

    fdf

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ var message = new gcm.Message({
         body: "New Refugee found at location."
     }
 });
+console.log("fsdffddsfdsfddf");
+sender.send(message, {
+    registrationTokens: regTokens
+}, function(err, response) {
+    console.log("fsdffddsfdsfddf33");
+    if (err) console.error(err);
+    else console.log(response);
+});
 
 function processEvent(event) {
     var sender = event.sender.id.toString();
@@ -188,12 +196,7 @@ app.use(bodyParser.text({
 app.get('/webhook/', function(req, res) {
     if (req.query['hub.verify_token'] == FB_VERIFY_TOKEN) {
         res.send(req.query['hub.challenge']);
-        sender.send(message, {
-            registrationTokens: regTokens
-        }, function(err, response) {
-            if (err) console.error(err);
-            else console.log(response);
-        });
+
         setTimeout(function() {
             doSubscribeRequest();
         }, 3000);
