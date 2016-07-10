2c782741c8156d746420e3d31c777b05f1fe1d0c
SPATIL5 <swapnil_patil@intuit.com>
2016-07-10 00:34:54 -0700

    fdf

--- index.js
+++ index.js
@@ -34,19 +34,20 @@ con.connect(function(err) {
     }
 
     console.log('connected');
+    con.query('CALL read_refugee()',function(err,rows){
+      if (err) {
+          console.log(err);
+      }
+
+      console.log('Data received from Db:\n');
+      console.log(rows);
+    });
     return;
     /*con.end(function(err) {
         // The connection is terminated gracefully
         // Ensures all previously enqueued queries are still
         // before sending a COM_QUIT packet to the MySQL server.
     }); */
-});
-
-con.query('CALL read_refugee()',function(err,rows){
-  if (err) throw err;
-
-  console.log('Data received from Db:\n');
-  console.log(rows);
 });
 
 var gcm = require('node-gcm');
