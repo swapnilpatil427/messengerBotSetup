abb3c8f9268e44ed9fe2210c35406e6f6232ff77
SPATIL5 <swapnil_patil@intuit.com>
2016-07-10 10:22:15 -0700

    sfdsd

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ function afterResponseData(action, response, responseText) {
                             console.log(err);
                         }
 
-                        console.log(rows.RowDataPacket[0]);
+                        console.log(rows.RowDataPacket[0].name);
                     });
                 });
 
