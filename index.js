2d7c37edfa9bd8c561ef942a3fb25936f394a483
SPATIL5 <swapnil_patil@intuit.com>
2016-07-10 04:15:49 -0700

    fg

--- index.js
+++ index.js
@@ -227,8 +227,29 @@ function sendFBMessage(sender, messageData, callback) {
                                 "title": "Other Organisation",
                                 "subtitle": "Provides Food",
                                 "Contact": 123455567,
-                                "distance": 25,
+                                "price": 25,
                                 "currency": "USD"
+                            }],
+                            "address": {
+                                "street_1": "1 Hacker Way",
+                                "street_2": "",
+                                "city": "Menlo Park",
+                                "postal_code": "94025",
+                                "state": "CA",
+                                "country": "US"
+                            },
+                            "summary": {
+                                "subtotal": 75.00,
+                                "shipping_cost": 4.95,
+                                "total_tax": 6.19,
+                                "total_cost": 56.14
+                            },
+                            "adjustments": [{
+                                "name": "New Customer Discount",
+                                "amount": 20
+                            }, {
+                                "name": "$10 Off Coupon",
+                                "amount": 10
                             }]
                         }
                     }
